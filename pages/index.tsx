d53190db7d8b0d50d694f096f6de772565b80ed8
lnardon <lucaslnardon@gmail.com>
2020-09-09 22:57:52 -0300

    Created basic project structure

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Typescript Next App</title>
+      </Head>
+      <h1>HELLO WORLD</h1>
+    </div>
+  )
+}
+
+export default Home
